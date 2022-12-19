2f4f2f2ff727897dc87292632bd3b5ffffbed005
juju941 <115978609+juju941@users.noreply.github.com>
2022-12-19 18:44:03 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -33,7 +33,7 @@ async def on_message(message):
         await message.channel.send(embed=embed)
 
     if message.content.startswith(f'{PREFIX}행운'):
-        embed=discord.Embed(title="행운 올려보기", description=message.author.name +"님 오늘은 좋은걸 얻을거 같아요!!", color=gold)
+        embed=discord.Embed(title="행운 올려보기", description=message.author.name +"님 오늘은 좋은걸 얻을거 같아요!!", color=0xffae00)
 
     if message.content.startswith(f'{PREFIX}가이드북'):
         await message.channel.send('https://cafe.naver.com/twguide/2?boardType=L')
